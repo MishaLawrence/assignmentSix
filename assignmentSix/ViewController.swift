165e9c42184436dba61af04fcb88dc931911d2c4
CM Student <cmstudent@UHWO-ACM-C-04.local>
2019-02-28 10:51:32 -1000

    Assignment 6 Completed (Going back to check on the final run)

--- assignmentSix/ViewController.swift
+++ assignmentSix/ViewController.swift
@@ -8,9 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myFriends.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        let text = myFriends[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    var myFriends = [ "Kylie", "Devin", "Jessica"]
+    
+override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -19,7 +32,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
